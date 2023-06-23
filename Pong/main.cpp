9914a9e987c038fc2968a29b0c252cfc7c1ac719
thuvu17 <112644941+thuvu17@users.noreply.github.com>
2023-06-23 11:25:00 -0400

    fixed header comment

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,7 +1,7 @@
 /**
 * Author: Thu Vu
-* Assignment: Simple 2D Scene
-* Date due: 2023-06-11, 11:59pm
+* Assignment: Pong Clone
+* Date due: 2023-06-23, 11:59pm
 * I pledge that I have completed this assignment without
 * collaborating with anyone else, in conformance with the
 * NYU School of Engineering Policies and Procedures on
